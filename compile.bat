92677b13bc66828cd79b0a3974e52250286a7fd2
Taylor Woll <taylor.woll@gmail.com>
2023-06-16 11:37:31 -0700

    Re-order segments and memory to get the rom layout correct

--- compile.bat
+++ compile.bat
@@ -18,4 +18,4 @@ cc65 -Oirs src/%name%.c -o build/%name%.s -g --add-source
 ca65 src/crt0.s -o build/crt0.o -g
 ca65 build/%name%.s -o build/%name%.o -g
 
-ld65 -C nrom_32k_vert.cfg -o build/%name%.nes build/crt0.o build/%name%.o nes.lib -Ln build/%name%.labels.txt --dbgfile build/%name%.dbg
+ld65 -C nrom_32k_vert.cfg -o build/%name%.nes build/crt0.o build/%name%.o nes.lib -Ln build/%name%.labels.txt --dbgfile build/%name%.dbg -v
